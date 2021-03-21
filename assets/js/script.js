d6084e5fab6e94ab9ab089b085b936179e77edd2
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-03-21 00:14:49 -0400

    ability to edit a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,13 +95,39 @@ var createTaskActions = function(taskId) {
 };
 
 var taskButtonHandler = function(event) {
-    console.log(event.target);
+    //get target element from event
+    var targetEl = event.target;
 
-    if (event.target.matches(".delete-btn")) {
+    //edit button was clicked
+    if (event.target.matches(".edit-btn")) {
+        //get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        editTask(taskId);
+    }    
+
+    //delete button was clicked
+    else if (event.target.matches(".delete-btn")) {
         //get the element's task id
         var taskId = event.target.getAttribute("data-task-id");
         deleteTask(taskId);
     }
+};
+
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+    
+    //get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    //get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    console.log(taskName);
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    console.log(taskType);
+
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
 };
 
 var deleteTask = function(taskId) {
